3a57fce32790353256674c6e9d8db4a08021ea28
Iris <iris.devillars@gmail.com>
2023-02-02 18:02:26 +0200

    update env variable name

--- hooks/contracts.ts
+++ hooks/contracts.ts
@@ -6,7 +6,7 @@ import { Provider } from 'starknet';
 export function useMixingContract() {
   const Mixing = new Contract(
     mix_abi as Abi,
-    process.env.NEXT_PUBLIC_NAMING_CONTRACT as string,
+    process.env.NEXT_PUBLIC_MIXING_CONTRACT as string,
     new Provider({
       sequencer: {
         network: 'goerli-alpha', // "mainnet-alpha"
