872e93875f169f4538fda3801da4198b9a997ae5
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2023-02-20 16:45:18 +0800

    first-pass; stuck at window undefined and dealing with loadable

--- components/ColorBlock.tsx
+++ components/ColorBlock.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent, useEffect, useState } from 'react';
 import { Color } from '../types/types';
 import styled from 'styled-components';
+import { PFive } from './P5';
 
 export type ColorMixProps = {
   color_1: Color;
@@ -12,14 +13,22 @@ export type ColorMixProps = {
 const ColorBlock: FunctionComponent<ColorMixProps> = ({color_1, color_2, ratio, res}: ColorMixProps) => {
   console.log('color_1', color_1)
 
+  if (typeof window == 'undefined') {
+    return null;
+  }
+
   return (
-    <ColorBlockStyled>
-      <div className="bloc1">
-          <div className="color1" style={{backgroundColor: `rgba(${color_1.r}, ${color_1.g}, ${color_1.b}, 1)`}}>({color_1.r}, {color_1.g}, {color_1.b})</div>
-          <div className="color2" style={{backgroundColor: `rgba(${color_2.r}, ${color_2.g}, ${color_2.b}, 1)`}}>({color_2.r}, {color_2.g}, {color_2.b})</div>
-      </div>
-      <div className="bloc2" style={{backgroundColor: `rgba(${res.r}, ${res.g}, ${res.b}, 1)`}}>({res.r}, {res.g}, {res.b})</div>
-    </ColorBlockStyled>
+    <>
+        <ColorBlockStyled>
+            <div className="bloc1">
+                <div className="color1" style={{backgroundColor: `rgba(${color_1.r}, ${color_1.g}, ${color_1.b}, 1)`}}>({color_1.r}, {color_1.g}, {color_1.b})</div>
+                <div className="color2" style={{backgroundColor: `rgba(${color_2.r}, ${color_2.g}, ${color_2.b}, 1)`}}>({color_2.r}, {color_2.g}, {color_2.b})</div>
+            </div>
+            <div className="bloc2" style={{backgroundColor: `rgba(${res.r}, ${res.g}, ${res.b}, 1)`}}>({res.r}, {res.g}, {res.b})</div>
+        </ColorBlockStyled>
+
+        <PFive />
+    </>
   );
 };
 
